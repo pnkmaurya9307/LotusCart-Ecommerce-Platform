16fbacd84b4557829b85670630aa700881a7ac82
Pankaj Mourya <pnkmaurya9307@gmail.com>
2026-06-23 18:24:14 +0530

    family: 4 is added in mailer .js

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -6,7 +6,8 @@ const transporter = nodemailer.createTransport({
     auth:{
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASS
-    }
+    },
+      family: 4  
 })
 
 export default transporter
